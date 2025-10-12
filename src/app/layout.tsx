8a900750dfca5c9c6bda0b4b7ba6563301f00ea0
Subhasish Nayak <subhasishnayak38@gmail.com>
2025-10-12 17:07:55 +0000

    Your commit message describing the changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       <head>
         <title>MyStudentHub</title>
         <meta name="description" content="Your all-in-one platform for academic success." />
-        <link rel="icon" href="/icon.png" />
+        <link rel="icon" href="/icon for studnet hub .jpg" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Playfair+Display:wght@400;700&display=swap" rel="stylesheet" />
